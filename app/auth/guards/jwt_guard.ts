eb1f7b50bb896c02cb92738741bca35d97ed3cc0
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-04-09 15:27:38 -0300

    feat: implements JwtGuard

---
+++ app/auth/guards/jwt_guard.ts
@@ -0,0 +1,106 @@
+import jwt from 'jsonwebtoken'
+import { JwtGuardOptions, JwtToken, JwtUserProviderContract } from '#types/guards'
+import { errors, symbols } from '@adonisjs/auth'
+import { AuthClientResponse, GuardContract } from '@adonisjs/auth/types'
+import { HttpContext } from '@adonisjs/core/http'
+
+export class JwtGuard<UserProvider extends JwtUserProviderContract<unknown>>
+  implements GuardContract<UserProvider[typeof symbols.PROVIDER_REAL_USER]>
+{
+  #userProvider: UserProvider
+  #options: JwtGuardOptions
+  #ctx: HttpContext
+
+  constructor(userProvider: UserProvider, options: JwtGuardOptions, ctx: HttpContext) {
+    this.#userProvider = userProvider
+    this.#options = options
+    this.#ctx = ctx
+  }
+
+  declare [symbols.GUARD_KNOWN_EVENTS]: {}
+
+  driverName: 'jwt' = 'jwt'
+
+  authenticationAttempted: boolean = false
+
+  isAuthenticated: boolean = false
+
+  user?: UserProvider[typeof symbols.PROVIDER_REAL_USER]
+
+  async generate(user: UserProvider[typeof symbols.PROVIDER_REAL_USER]): Promise<JwtToken> {
+    const providerUser = await this.#userProvider.createUserForGuard(user)
+    const token = jwt.sign({ userId: providerUser.getId() }, this.#options.secret)
+    return {
+      type: 'Bearer',
+      token,
+    }
+  }
+
+  async authenticate(): Promise<UserProvider[typeof symbols.PROVIDER_REAL_USER]> {
+    if (this.authenticationAttempted) {
+      return this.getUserOrFail()
+    }
+    this.authenticationAttempted = true
+
+    const authHeader = this.#ctx.request.header('authorization')
+    if (!authHeader) {
+      throw new errors.E_UNAUTHORIZED_ACCESS('Unauthorized access', {
+        guardDriverName: this.driverName,
+      })
+    }
+
+    const [, token] = authHeader.split('Bearer ')
+    if (!token) {
+      throw new errors.E_UNAUTHORIZED_ACCESS('Unauthorized access', {
+        guardDriverName: this.driverName,
+      })
+    }
+
+    const payload = jwt.verify(token, this.#options.secret)
+    if (typeof payload !== 'object' || !('userId' in payload)) {
+      throw new errors.E_UNAUTHORIZED_ACCESS('Unauthorized access', {
+        guardDriverName: this.driverName,
+      })
+    }
+
+    const providerUser = await this.#userProvider.findById(payload.userId)
+    if (!providerUser) {
+      throw new errors.E_UNAUTHORIZED_ACCESS('Unauthorized access', {
+        guardDriverName: this.driverName,
+      })
+    }
+
+    this.user = providerUser.getOriginal()
+    return this.getUserOrFail()
+  }
+
+  async check(): Promise<boolean> {
+    try {
+      await this.authenticate()
+      return true
+    } catch {
+      return false
+    }
+  }
+
+  getUserOrFail(): UserProvider[typeof symbols.PROVIDER_REAL_USER] {
+    if (!this.user) {
+      throw new errors.E_UNAUTHORIZED_ACCESS('Unauthorized access', {
+        guardDriverName: this.driverName,
+      })
+    }
+
+    return this.user
+  }
+
+  async authenticateAsClient(
+    user: UserProvider[typeof symbols.PROVIDER_REAL_USER]
+  ): Promise<AuthClientResponse> {
+    const token = await this.generate(user)
+    return {
+      headers: {
+        authorization: `Bearer ${token.token}`,
+      },
+    }
+  }
+}
