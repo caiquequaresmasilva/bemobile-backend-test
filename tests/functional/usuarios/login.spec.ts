37f29357cf2141f18ab92083751940f655cc5e6b
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-04-14 11:02:39 -0300

    test: make functional tests for "usuarios" routes

---
+++ tests/functional/usuarios/login.spec.ts
@@ -0,0 +1,29 @@
+import Usuario from '#models/usuario'
+import { USUARIO } from '#tests/mocks/usuario'
+import testUtils from '@adonisjs/core/services/test_utils'
+import { test } from '@japa/runner'
+
+test.group('Usuarios login', (group) => {
+  const ENDPOINT = '/usuarios/login'
+  group.setup(async () => {
+    await Usuario.create(USUARIO)
+  })
+  group.teardown(() => testUtils.db().truncate())
+  test('Retorna token de usuário logado', async ({ client }) => {
+    const response = await client.post(ENDPOINT).json(USUARIO)
+    response.assertAgainstApiSpec()
+    response.assertBodyContains({
+      data: {
+        type: 'Bearer',
+      },
+    })
+  })
+  test('Retorna status "400" ao logar com email inválido', async ({ client }) => {
+    const response = await client.post(ENDPOINT).json({ ...USUARIO, email: 'email@' })
+    response.assertStatus(400)
+  })
+  test('Retorna status "400" ao logar com senha inválida', async ({ client }) => {
+    const response = await client.post(ENDPOINT).json({ ...USUARIO, senha: 'senha' })
+    response.assertStatus(400)
+  })
+})
---
+++ tests/functional/usuarios/signup.spec.ts
@@ -0,0 +1,34 @@
+import { USUARIO } from '#tests/mocks/usuario'
+import testUtils from '@adonisjs/core/services/test_utils'
+import { test } from '@japa/runner'
+
+test.group('Usuarios signup', (group) => {
+  const ENDPOINT = '/usuarios'
+  group.teardown(() => testUtils.db().truncate())
+  test('Registra um novo usuário e retorna seus dados', async ({ client }) => {
+    const response = await client.post(ENDPOINT).json(USUARIO)
+    response.assertAgainstApiSpec()
+    response.assertBodyContains({
+      data: {
+        email: USUARIO.email,
+      },
+    })
+  })
+  test('Retorna status "422" caso o email informado esteja no formato incorreto', async ({
+    client,
+  }) => {
+    const response = await client.post(ENDPOINT).json({ ...USUARIO, email: '@.com' })
+    response.assertStatus(422)
+  })
+  test('Retorna status "422" caso a senha informada esteja no formato incorreto', async ({
+    client,
+  }) => {
+    const response = await client.post(ENDPOINT).json({ ...USUARIO, senha: 'senha' })
+    response.assertStatus(422)
+  })
+
+  test('Retorna status "400" caso o email já esteja registrado no sistema', async ({ client }) => {
+    const response = await client.post(ENDPOINT).json(USUARIO)
+    response.assertStatus(400)
+  })
+})
